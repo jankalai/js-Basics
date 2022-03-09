b19d0153505fdbf2f775a132c7b9a1f9a1b3bf43
Janani <janani.it02@gmail.com>
2022-03-08 19:06:56 -0500

    my learning file

---
+++ hello.js
@@ -0,0 +1,40 @@
+console.log('hello world')
+var name = "jan"
+var age = 10
+console.log(age)
+var num1 = 10
+var num2 = 30
+console.log(num1 + num2)
+console.log(100 + 20)
+var name = 'jan'
+console.log(name);
+
+var name = 'gow'
+console.log(name);
+
+var age = 100
+console.log(age)
+
+var country = 'India'
+console.log('I am from ' + country);
+
+var x = 'X'
+var y = 'Y'
+console.log(x + y)
+
+
+var num1 = 10
+var num2 = 20
+console.log(num1 + num2)
+
+console.log('Hello world')
+
+var name = "jan"
+console.log(name)
+var age = 10
+console.log(age)
+
+var num1 = 10
+var num2 = 30
+console.log(num1 + num2)
+console.log(100 * 70) 
